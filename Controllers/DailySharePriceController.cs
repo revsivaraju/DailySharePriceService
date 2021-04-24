1476b36ff1a4fec051eecd98c65efe04a5a546b1
ediga divya <edivyasai99@gmail.com>
2021-04-24 11:40:29 +0530

    secondcommit

---
+++ Controllers/DailySharePriceController.cs
@@ -0,0 +1,42 @@
+﻿using DailySharePrice.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DailySharePrice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DailySharePriceController : ControllerBase
+    {
+        private readonly IInMemoryStockDb stockDbObj;
+
+        public DailySharePriceController(IInMemoryStockDb obj)
+        {
+            stockDbObj = obj;
+        }
+
+        // GET: api/<DailySharePriceController>
+        [HttpGet("{stockName}")]
+        public ActionResult Get(string stockName)
+        {
+            if (stockName == null)
+            {
+                return BadRequest();
+            }
+            var obj = stockDbObj.DailySharePrice(stockName);
+            if (obj == null)
+            {
+                return BadRequest();
+            }  
+          return Ok(obj);
+        }
+
+        
+        
+    }
+}
---
+++ Models/IInMemoryStockDb.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DailySharePrice.Models
+{
+   public  interface IInMemoryStockDb
+    {
+        DailyStockDetails DailySharePrice(string StockName);
+    }
+}
