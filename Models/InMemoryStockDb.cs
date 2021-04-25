29da8ba2ef56a024b938841251d0bab55918d9e5
edivyasai <60880915+edivyasai@users.noreply.github.com>
2021-04-25 22:51:17 +0530

    Update InMemoryStockDb.cs

--- Models/InMemoryStockDb.cs
+++ Models/InMemoryStockDb.cs
@@ -8,11 +8,11 @@ namespace DailySharePrice.Models
     public class InMemoryStockDb : IInMemoryStockDb
     {
         public static List<DailyStockDetails> dailyStockDetails = new List<DailyStockDetails> {
-            new DailyStockDetails { StockId=1,StockName="ABC",StockValue=100},
-            new DailyStockDetails { StockId = 2, StockName = "DEF", StockValue = 200 },
-            new DailyStockDetails { StockId = 3, StockName = "GHI", StockValue = 300 },
-            new DailyStockDetails { StockId = 4, StockName = "JKL", StockValue = 400 },
-            new DailyStockDetails { StockId = 5, StockName = "MNO", StockValue = 500 }
+            new DailyStockDetails { StockId=1,StockName="HDFC",StockValue=100},
+            new DailyStockDetails { StockId = 2, StockName = "CDSL", StockValue = 200 },
+            new DailyStockDetails { StockId = 3, StockName = "Pidilite", StockValue = 300 },
+            new DailyStockDetails { StockId = 4, StockName = "SBI", StockValue = 400 },
+            new DailyStockDetails { StockId = 5, StockName = "coforge", StockValue = 500 }
             };
 
         public DailyStockDetails DailySharePrice(string stockName)
